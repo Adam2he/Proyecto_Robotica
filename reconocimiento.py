e37dea28de16d63d3be717a311caec8eac21fcd0
Adam2he <adam.rg.2h@gmail.com>
2023-12-01 19:31:35 +0100

    Cambios

--- reconocimiento.py
+++ reconocimiento.py
@@ -1,51 +1,104 @@
-import cv2
 import face_recognition
+import cv2
+import numpy as np
 
-# Imagen a comparar
-image = cv2.imread("Images/Gaby.jpg")
-face_loc = face_recognition.face_locations(image)[0]
-#print("face_loc:", face_loc)
-face_image_encodings = face_recognition.face_encodings(image, known_face_locations=[face_loc])[0]
-print("face_image_encodings:", face_image_encodings)
-'''
-cv2.rectangle(image, (face_loc[3], face_loc[0]), (face_loc[1], face_loc[2]), (0, 255, 0))
-cv2.imshow("Image", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()'''
+# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
+# other example, but it includes some basic performance tweaks to make things run a lot faster:
+#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
+#   2. Only detect faces in every other frame of video.
 
-######################################################################################
-# Video Streaming
+# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
+# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+# Get a reference to webcam #0 (the default one)
+video_capture = cv2.VideoCapture(0)
+
+# Load a sample picture and learn how to recognize it.
+obama_image = face_recognition.load_image_file("obama.jpg")
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+# Load a second sample picture and learn how to recognize it.
+biden_image = face_recognition.load_image_file("biden.jpg")
+biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+
+# Create arrays of known face encodings and their names
+known_face_encodings = [
+    obama_face_encoding,
+    biden_face_encoding
+]
+known_face_names = [
+    "Barack Obama",
+    "Joe Biden"
+]
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
 
 while True:
-     ret, frame = cap.read()
-     if ret == False: break
-     frame = cv2.flip(frame, 1)
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
 
-     face_locations = face_recognition.face_locations(frame, model="cnn")
-     if face_locations != []:
-          for face_location in face_locations:
-               face_frame_encodings = face_recognition.face_encodings(frame, known_face_locations=[face_location])[0]
-               result = face_recognition.compare_faces([face_image_encodings], face_frame_encodings)
-               #print("Result:", result)
+    # Only process every other frame of video to save time
+    if process_this_frame:
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-               if result[0] == True:
-                    text = "Gaby"
-                    color = (125, 220, 0)
-               else:
-                    text = "Desconocido"
-                    color = (50, 50, 255)
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
 
-               cv2.rectangle(frame, (face_location[3], face_location[2]), (face_location[1], face_location[2] + 30), color, -1)
-               cv2.rectangle(frame, (face_location[3], face_location[0]), (face_location[1], face_location[2]), color, 2)
-               cv2.putText(frame, text, (face_location[3], face_location[2] + 20), 2, 0.7, (255, 255, 255), 1)
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
+        face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
 
-     cv2.imshow("Frame", frame)
-     k = cv2.waitKey(1)
-     if k == 27 & 0xFF:
-          break
+            # # If a match was found in known_face_encodings, just use the first one.
+            # if True in matches:
+            #     first_match_index = matches.index(True)
+            #     name = known_face_names[first_match_index]
 
-cap.release()
+            # Or instead, use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+
+            face_names.append(name)
+
+    process_this_frame = not process_this_frame
+
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        print(str(left)+","+str(top)+","+str(right)+","+str(bottom))
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
 cv2.destroyAllWindows()
